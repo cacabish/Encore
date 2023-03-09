21169d1198748ebe89e9f179d9348f4ab7d6b1ba
cacabish <25556130+cacabish@users.noreply.github.com>
2023-03-09 00:31:47 -0700

    Add ability to create desired .mus files

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,145 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createAndDownloadBlobFile } from './mus/createmus';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        const form = e.target as typeof e.target & {
+            songTitle: { value: string };
+            gameTitle: { value: string };
+            composer: { value: string };
+            arranger: { value: string };
+            company: { value: string };
+            year: { value: string };
+        };
+        const title = form['songTitle'].value;
+        const game = form['gameTitle'].value;
+        const composer = form['composer'].value;
+        const arranger = form['arranger'].value;
+        const company = form['company'].value;
+        const year = form['year'].value;
+
+        createAndDownloadBlobFile(title, game, composer, arranger, company, year);
+    }
+
+    return (
+        <form
+            id='mainForm'
+            method='post'
+            onSubmit={handleSubmit}
+            autoComplete='off'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <fieldset>
+                <legend>Song Details:</legend>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label htmlFor='youtubeLink'>YouTube Link:</label>
+                            </td>
+                            <td colSpan={3}>
+                                <input
+                                    type='text'
+                                    id='youtubeLink'
+                                    name='youtubeLink'
+                                    placeholder='https://www.youtube.com/watch?v=fC7oUOUEEi4'
+                                    // pattern='(?:https?:\/\/)?(?:www\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[-a-zA-Z0-9_]{11,}(?!\S))\/)|(?:\S*v=|v\/)))([-a-zA-Z0-9_]{11,})'
+                                    title='YouTube URL'
+                                    required
+                                ></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label htmlFor='songTitle'>Song Title:</label>
+                            </td>
+                            <td>
+                                <input
+                                    type='text'
+                                    id='songTitle'
+                                    name='songTitle'
+                                    pattern='^".+"$'
+                                    title='Starts and ends with quotes (")'
+                                    required
+                                ></input>
+                            </td>
+                            <td>
+                                <label htmlFor='gameTitle'>Game Title:</label>
+                            </td>
+                            <td>
+                                <input
+                                    type='text'
+                                    id='gameTitle'
+                                    name='gameTitle'
+                                    required
+                                ></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label htmlFor='composer'>Composer:</label>
+                            </td>
+                            <td>
+                                <input
+                                    type='text'
+                                    id='composer'
+                                    name='composer'
+                                    required
+                                ></input>
+                            </td>
+                            <td>
+                                <label htmlFor='arranger'>Arranger:</label>
+                            </td>
+                            <td>
+                                <input
+                                    type='text'
+                                    id='arranger'
+                                    name='arranger'
+                                    required
+                                ></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label htmlFor='company'>Company:</label>
+                            </td>
+                            <td>
+                                <input
+                                    type='text'
+                                    id='company'
+                                    name='company'
+                                    required
+                                ></input>
+                            </td>
+                            <td>
+                                <label htmlFor='year'>Year:</label>
+                            </td>
+                            <td>
+                                <input
+                                    type='number'
+                                    id='year'
+                                    name='year'
+                                    min={1900}
+                                    max={2100}
+                                    step={1}
+                                    defaultValue={2023}
+                                    required
+                                ></input>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br />
+                <input
+                    type='submit'
+                    value='Submit'
+                    required
+                ></input>
+            </fieldset>
+        </form>
+    );
 }
 
 export default App;
