65f5fa652e0e670aaae4ace6807100bee551e3a5
cacabish <25556130+cacabish@users.noreply.github.com>
2023-03-08 19:06:45 -0700

    Updates

---
+++ precache-manifest.e65e346df219d05c079b5baeb8eeed15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f8b422dd482e26258f",
+    "url": "/encore/static/css/main.6e8ddca3.chunk.css"
+  },
+  {
+    "revision": "21f8b422dd482e26258f",
+    "url": "/encore/static/js/main.7a1f5601.chunk.js"
+  },
+  {
+    "revision": "9a28e584c62fe3207005",
+    "url": "/encore/static/js/runtime~main.7cd82cf3.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/encore/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/encore/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/encore/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "31b89110ee4db1e3eb6cb44cab5ec51b",
+    "url": "/encore/index.html"
+  }
+];
